6013404771dcc27424343ee837efef2e5598dce6
Muaaz [Muhammadaziz] <muhammadaziz.erg.1229@gmail.com>
2024-01-31 22:43:56 +0500

    createSlice used

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,45 +1,25 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-import { findIndex } from "lodash";
+import { createSlice } from "@reduxjs/toolkit";
 
-// actions
-export const bugAdded = createAction('bugAdded');
-export const bugResolved = createAction('bugResolved');
-export const bugRemoved = createAction('bugRemoved');
-
-// reducer
 let last = 0;
 
-createReducer([], {
-    [bugAdded.type]: (bugs, action) => {
-        bugs.push({
-            id: ++last,
-            description: action.payload.description,
-            resolved: false
-        })
-    },
-    [bugResolved.type]: (bugs, action) => {
-        const index = findIndex(bug => bug.id === action.payload.id);
-        bugs[index].resolved = true;
-    }
-})
-
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case bugAdded.type:
-      return [
-        ...state,
-        {
-            id: ++last,
-            description: action.payload.description,
-            resolved: false
+const slice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducers: {
+        bugAdded: (bugs, action) => {
+            bugs.push({
+                id: ++last,
+                description: action.payload.description,
+                resolved: false
+            })
+        },
+        bugResolved: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id);
+            bugs[index].resolved = true;
         }
-      ];
-    case bugRemoved.type:
-      return state.filter(bug => bug.id !== action.payload.id);
-    case bugResolved.type:
-      return state.map(bug =>
-        (bug.id === action.payload.id) ? {...bug, resolved: true} : bug);
-    default:
-      return state;
-  }
-}
+    }
+});
+
+export const {bugAdded, bugResolved} = slice.actions;
+
+export default slice.reducer;
